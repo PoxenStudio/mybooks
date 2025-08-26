53b9f7fbbd6e3c8e759e0206d2e1e678b95f1ff5
HorkyChen <horky.chen@gmail.com>
2025-08-26 09:24:45 +0800

    封面限制在1M以内

--- app/src/pages/book/_bookid.vue
+++ app/src/pages/book/_bookid.vue
@@ -1006,7 +1006,7 @@ export default {
             return;
           }
           const file = this.cover_file;
-          if (file.size > 256 * 1024) {
+          if (file.size > 1024 * 1024) {
             this.cover_error = this.$t('book.coverTooLarge');
             return;
           }
