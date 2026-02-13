8e40ec5de27e4631ab0802ba60068b3c23bd394d
horky <horky.chen@gmail.com>
2026-02-13 08:00:23 +0800

    更新图书管理在移动端上的显示

--- webserver/utils.py
+++ webserver/utils.py
@@ -62,7 +62,7 @@ class SimpleBookFormatter:
             "languages": self.val("languages", None),
             "isbn": self.val("isbn", None),
             "img": self.cdn_url + "/get/cover/%(id)s.jpg?t=%(ts)s" % b,
-            "thumb": self.cdn_url + "/get/thumb_60x80/%(id)s.jpg?t=%(ts)s&size=60x80" % b,
+            "thumb": self.cdn_url + "/get/thumb_120x160/%(id)s.jpg?t=%(ts)s&size=120x160" % b,
             # 额外填充的字段
             "collector": self.get_collector(),
             "count_visit": self.val("count_visit", 0),
